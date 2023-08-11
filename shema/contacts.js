1aa0a1d9180a4f54c4169a83285940f01bf36242
Maksym Kravchenko <kmaksym@gmail.com>
2023-08-11 11:00:32 -0700

    JOI validation email phone done

--- shema/contacts.js
+++ shema/contacts.js
@@ -2,10 +2,12 @@ const Joi = require("joi");
 
 const addSchema = Joi.object({
   name: Joi.string().required(),
-  phone: Joi.string().required(),
-  email: Joi.string().required(),
+  phone: Joi.string()
+    .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
+    .required(),
+  email: Joi.string().email().required(),
 });
 
 module.exports = {
-    addSchema,
-}
+  addSchema,
+};
