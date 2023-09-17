d68680077612f5baf876103550948219fd961890
Maksym Kravchenko <kmaksym@gmail.com>
2023-09-17 12:28:40 -0700

    09/17 email/2

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,20 +1,20 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const { META_PASSWORD } = process.env;
+const { META_PASSWORD, EMAIL } = process.env;
 
 const nodemailerConfig = {
     host: "smtp.meta.ua",
     port: "465",
     secure: true,
     auth: {
-        user: "m9496179336k@meta.ua",
+        user: EMAIL,
         pass: META_PASSWORD,
     }
 }
 const transport = nodemailer.createTransport(nodemailerConfig);
 const sendEmail = async (data) => {
-  const email = { ...data, from: "m9496179336k@meta.ua" };
+  const email = { ...data, from: EMAIL };
  
   await transport.sendMail( email);
   return true;
